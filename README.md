ad9a220d1cd28fe5b87052245173b9467f657f2c
caifu_desk <caifu@email.com>
2026-02-13 13:42:57 +0800

    feat(protocol update)update algo type protocol

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # mini_serial_demo
-serial demo for baton_mini
+这是一个简易的mini串口收发控制的demo，目前使用ros1发布的mini的里程计话题，未做ros2的适配。
+
+## 通讯参数
+UART的接口为GH1.0的5pin，从螺丝孔那里开始从左到右分别是GND、TRIG、TX、RX、5V，波特率：115200，3.3v电平
+
+## build && run
+
+```
+mkdir -p ~/mini_serial_demo
+cd ~/mini_serial_demo
+git clone https://github.com/Hessian-matrix/mini_serial_demo
+catkin_make
+source devel/setup.bash
+roslaunch serial_demo serial_demo.launch
+```
+
+## 运行效果
+运行后终端等待用户的按键输入：[ INFO] [1770238508.251035632]: please input command: 1-start, 2-stop, 3-reset, 4-exit, a-switch algo
+- 1： 启动算法
+- 2： 停止算法
+- 3： 重置算法
+- 4： 退出程序
+- a:  切换算法
--- serial_demo/src/serial_demo.cpp
+++ serial_demo/src/serial_demo.cpp
@@ -8,7 +8,8 @@
 //demo 程序
 //功能： 1) 发送状态控制指令到Mini控制启停算法等
 //       2)解码Mini发送的姿态数据,打印出来或者打包成ROS话题发送出去
-
+//协议： 1)发送控制指令协议：帧头0x67 0x28 + 1字节控制位指令 +1字节algo_type指令 + 1字节校验 + 0x09 0x0d
+//       2)接收数据协议：帧头0x66 0x27 + 4字节帧ID + 28字节姿态数据 + 24字节速度数据 + 1字节校验 + 0x09 0x0d
 #define LREV_LEN    61
 #define LSEND_LEN   34
 #define LS_CHECK    LSEND_LEN - 3   //发送校验位序列
@@ -42,6 +43,10 @@ enum algo_state{
     algo_disable = 0x02,    //停止
     algo_restart = 0x03,    //重置
     algo_none   = 0x00,
+};
+enum algo_type{
+    algo_3 = 0x00,     //stereo3算法
+    algo_4 = 0x01,     //stereo4算法
 };
 
 //发布里程计话题
@@ -103,11 +108,13 @@ void unpack_uart(odom_t& odom,int& frame_id){
 
 /// @brief 发送控制指令
 /// @param t algo_state 控制指令
-void send_command(const algo_state t){   
+/// @param algo algo_type 算法类型，默认stereo3
+void send_command(const algo_state t, const algo_type algo = algo_3){   
     memset(send_buffer,0,LSEND_LEN);
     send_buffer[0] = 0x67;
     send_buffer[1] = 0x28;
     send_buffer[2] = t;
+    send_buffer[3] = algo;
     char check_sum = 0;
     for(int i = 2;i < LS_CHECK;i++){
         check_sum += send_buffer[i];
@@ -167,26 +174,49 @@ int main(int argc,char **argv){
     pub_odom = nh.advertise<nav_msgs::Odometry>("/baton_mini_odometry", 10);
 
     std::thread receive_thread{serial_receive_thread};
-    int v;
+    algo_type algo = algo_3;
+    char v;
     ros::Rate r(10);
+    bool algo_running = false;
+    std::string algo_str="stereo3";
     while(ros::ok()){
-        ROS_INFO("please input command:");
+        ROS_INFO("please input command: 1-start, 2-stop, 3-reset, 4-exit, a-switch algo");
         std::cin >> v;
-        if(v == 1){
-            ROS_INFO("algo_enable");
-            send_command(algo_enable);
+        if(v == '1'){
+            ROS_INFO("%s algo start", algo_str.c_str());
+            send_command(algo_enable, algo);
+            algo_running = true;
         }
-        else if(v == 2){
-            ROS_INFO("algo_disable");
-            send_command(algo_disable);
+        else if(v == '2'){
+            ROS_INFO("%s algo stop", algo_str.c_str());
+            send_command(algo_disable, algo);
+            algo_running = false;
         }
-        else if(v == 3){
-            ROS_INFO("algo_reset");
-            send_command(algo_restart);
+        else if(v == '3'){
+            ROS_INFO("%s algo_reset",algo_str.c_str());
+            send_command(algo_restart, algo);
         }
-        else if(v == 4){
+        else if(v == '4'){
             ROS_INFO("exit");
             break;
+        }
+        else if(v == 'a')//停止算法时可切换算法
+        {
+           if(!algo_running)
+           {
+                if(algo == algo_type::algo_3)
+                {
+                    algo = algo_type::algo_4;
+                    algo_str = "stereo4";
+                    ROS_INFO("switch to %s", algo_str.c_str());
+                }
+                else
+                {
+                    algo = algo_type::algo_3;
+                    algo_str = "stereo3";
+                    ROS_INFO("switch to %s", algo_str.c_str());
+                }   
+           }
         }
         r.sleep();
         ros::spinOnce(); 
